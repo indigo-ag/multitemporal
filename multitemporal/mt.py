8c1d8b23a44e1f392aafaec90805a698f0df3d5c
Damien Sulla-Menashe <damien@telluslabs.com>
2019-11-15 14:39:56 -0500

    found an unintentional typo

--- multitemporal/mt.py
+++ multitemporal/mt.py
@@ -520,7 +520,7 @@ def main():
     if not conf['nongips']:
         run_func = run_gipsexport
     try:
-        --n
+        run_func(**conf)
     except Exception as e:
         from pprint import pformat
         import traceback
